fcdf86ab524d63d6c4cfbd4db9ce6e82d14872b4
Brian Pham <brianlpham21@gmail.com>
2018-08-16 12:21:18 -0700

    styling for image logo

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ class App extends Component {
         {/* navigation bar */}
         <header>
           <div className="app-header">
-            <img src='https://www.mymedicarebot.com/mmb/wp-content/uploads/2018/07/lgoooooooo.png' alt="mymedicarebot-logo" className="header-image" />
+            <a href="https://www.mymedicarebot.com/" target="_blank" rel="noopener noreferrer"><img src='https://www.mymedicarebot.com/mmb/wp-content/uploads/2018/07/lgoooooooo.png' alt="mymedicarebot-logo" className="header-image" /></a>
             <ul className="app-header-items">
               <li><a href="https://www.mymedicarebot.com/" target="_blank" rel="noopener noreferrer">mymedicarebot</a></li>
               <li><a href="https://github.com/brianlpham21/mymedicarebot-challenge">about</a></li>
